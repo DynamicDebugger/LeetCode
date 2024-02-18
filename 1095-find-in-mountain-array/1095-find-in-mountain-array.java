7d16941d156c05d9758aea99e4e6dee678695eff
Balasubramani E <138235393+ishhookayy@users.noreply.github.com>
2024-02-18 15:49:58 +0530

    Time: 0 ms (100.00%), Space: 43.7 MB (9.97%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,75 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        
+        int peek = peekOfMountain(mountainArr);
+        
+        int elementIndex = binarySearch(mountainArr, 0, peek, target, true);
+        
+        if(elementIndex == -1){
+            elementIndex = binarySearch(mountainArr, peek, mountainArr.length() - 1, target, false);
+        }
+        
+        return elementIndex;
+    }
+
+    //peekOfMountain ~ method to return peek element index
+    public int peekOfMountain (MountainArray mountainArr){
+     
+        int start = 0;
+        int end = mountainArr.length() - 1;
+        
+        while(start < end){
+            
+            int mid = start + (end - start) / 2;
+            
+            if(mountainArr.get(mid) > mountainArr.get(mid+1)){
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+        return start;
+        
+    }
+    //binarySearch
+    public int binarySearch(MountainArray mountainArr,int start,int end,int target,boolean isAsc) {
+        
+    	 while (start <= end) {
+             int mid = start + (end - start) / 2;
+
+             
+             if(target == mountainArr.get(mid)){
+                 return mid;
+             }
+             
+             if(isAsc){
+                 
+                 if (target < mountainArr.get(mid)) {
+                     end = mid - 1;
+                 }else{
+                     start = mid + 1;
+                 }
+                 
+             }else{
+                 
+                 if (target > mountainArr.get(mid)) {
+                     end = mid - 1;
+                 }else{
+                     start = mid + 1;
+                 }
+                 
+             }
+         }
+     	
+     	return -1;
+    }
+}

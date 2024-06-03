ee127a8c4f48f2e5a99e2d42921609382d3f65da
Balasubramani E <138235393+DynamicDebugger@users.noreply.github.com>
2024-06-03 23:47:17 +0530

    Time: 2 ms (94.76%), Space: 46.6 MB (27.56%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int[] arr = new int[n+1];
+        
+        for(int i = 0 ; i <= n ; i++){
+            arr[i] = noOfSetBitsN(i);
+        }
+        return arr;
+    }
+
+    public int noOfSetBitsN(int n){
+        int count = 0;
+        while(n > 0){
+            count++;
+            n = n - (n & -n);
+        }
+        return count;
+    }
+}

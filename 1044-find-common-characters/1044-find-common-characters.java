8319458f8daa9179d9a4e01b51b8184d04c79a3d
Balasubramani E <138235393+DynamicDebugger@users.noreply.github.com>
2024-06-06 01:30:49 +0530

    Time: 19 ms (21.65%), Space: 44.6 MB (22.06%) - LeetHub

--- 1044-find-common-characters/1044-find-common-characters.java
+++ 1044-find-common-characters/1044-find-common-characters.java
@@ -1,48 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class Solution {
     public List<String> commonChars(String[] words) {
-        
-        Map<Character,Integer> freqMap = new HashMap<>();
+        Map<Character, Integer> freqMap = new HashMap<>();
 
-        for(int i = 0 ; i < words[0].length() ; i++){
-            char c = words[0].charAt(i);
-            if(!freqMap.containsKey(c)){
-                freqMap.put(c,1);
-            }else{
-                freqMap.put(c,freqMap.get(c)+1);
-            }
+        // Initialize the frequency map with the first word
+        for (char c : words[0].toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
         }
 
-        for(int i = 1 ; i < words.length ; i++){
-            Map<Character,Integer> map = new HashMap<>();
-            for(int j = 0 ; j < words[i].length() ; j++){
-                char c = words[i].charAt(j);
-                if(!map.containsKey(c)){
-                    map.put(c,1);
-                }else{
-                    map.put(c,map.get(c)+1);
+        // Update the frequency map with the minimum frequencies
+        for (int i = 1; i < words.length; i++) {
+            Map<Character, Integer> currentMap = new HashMap<>();
+            for (char c : words[i].toCharArray()) {
+                currentMap.put(c, currentMap.getOrDefault(c, 0) + 1);
+            }
+
+            // Retain only the minimum frequency of each character
+            for (char key : freqMap.keySet()) {
+                if (currentMap.containsKey(key)) {
+                    freqMap.put(key, Math.min(freqMap.get(key), currentMap.get(key)));
+                } else {
+                    freqMap.put(key, 0); // Ensure characters not in current word are set to 0
                 }
             }
-            for(Map.Entry<Character,Integer> em: freqMap.entrySet()){
-                char key = em.getKey();int val = em.getValue();
-                if(map.containsKey(key)){
-                    freqMap.put(key,(map.get(key) < val) ?map.get(key):val);
-                }else{
-                    freqMap.put(key,0);
-                }
-            }
-           
         }
 
-        List<String> list = new ArrayList<>();
-        for(Map.Entry<Character,Integer> em: freqMap.entrySet()){
-            char key = em.getKey();int val = em.getValue();
-
-            for(int j = 0 ; j < val ; j++){
-                list.add(String.valueOf(key));
+        // Prepare the list of common characters
+        List<String> result = new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+            char key = entry.getKey();
+            int val = entry.getValue();
+            for (int j = 0; j < val; j++) {
+                result.add(String.valueOf(key));
             }
         }
 
-        return list;
-
+        return result;
     }
 }

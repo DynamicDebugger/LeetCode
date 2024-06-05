72050fac09bb6597e8080d423aab26de3e187d83
Balasubramani E <138235393+DynamicDebugger@users.noreply.github.com>
2024-06-06 00:01:01 +0530

    Time: 5 ms (54.35%), Space: 42.6 MB (91.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,33 +2,16 @@ class Solution {
     public boolean isAnagram(String s, String t) {
 
         if(s.length() != t.length())    return false;
-        Map<Character,Integer> map1 = new HashMap<>();
-        for(int i = 0 ; i < s.length() ; i++){
-            char c = s.charAt(i);
-            if(!map1.containsKey(c)){
-                map1.put(c,1);
-                continue;
-            }
-            map1.put(c,map1.get(c)+1);
+
+        int[] count = new int[26];
+        for(int i = 0 ; i < s.length() ;i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
 
-        for(int i = 0 ; i < t.length() ; i++){
-            char c = t.charAt(i);
-            if(map1.containsKey(c)){
-                map1.put(c,map1.get(c)-1);
-            }else{
-                map1.put(c,1);
-            }
-            
+        for(int n: count){
+            if(n != 0) return false;
         }
-
-        for(Map.Entry<Character,Integer> map : map1.entrySet()){
-            char key = map.getKey();int val = map.getValue();
-            if(val != 0){
-                return false;
-            }
-        }
-
         return true;
     }
 }

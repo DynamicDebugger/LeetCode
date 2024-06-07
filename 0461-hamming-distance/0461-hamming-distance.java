745b3db4be5758fda63aed5f47c4f2e6d7e37641
Balasubramani E <138235393+DynamicDebugger@users.noreply.github.com>
2024-06-08 00:11:01 +0530

    Time: 0 ms (100%), Space: 40.8 MB (5.22%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int z = x ^ y;
+        int count = 0 ; 
+        while(z > 0){
+            count++;
+            // z = z - (z & -z);
+            z = z & (z - 1);
+        }
+
+        return count;
+    }
+}

c0ddf328adab000c299989766466b2569546200b
Balasubramani E <138235393+DynamicDebugger@users.noreply.github.com>
2024-10-20 15:10:19 +0530

    Time: 0 ms (100%), Space: 40.9 MB (6.49%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, - 1);
+        return climbStair(n, dp);       
+    }
+    private int climbStair(int n, int[] dp){
+        if(n == 0)  return 1;  
+        if(n < 0)   return 0;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = climbStair(n - 1, dp)+ climbStair(n - 2, dp);
+    }
+}

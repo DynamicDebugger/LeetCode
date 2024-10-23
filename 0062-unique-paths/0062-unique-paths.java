188cb4aa0383d5d11ab930ebc45706ee84a17ea9
Balasubramani E <138235393+DynamicDebugger@users.noreply.github.com>
2024-10-23 14:20:13 +0530

    Time: 0 ms (100%), Space: 40.6 MB (19.23%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,21 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        
         int[][] dp = new int[m][n];
-        for(int[] row: dp) Arrays.fill(row, -1);
-        return totUniqPath(m -1, n -1, dp);        
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+
+                if(i == 0  && j == 0) dp[i][j] = 1;
+                else{
+                    int up = 0, left = 0;
+                    if(i > 0) up = dp[i - 1][j];
+                    if(j > 0) left = dp[i][j - 1];
+
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
     }
-    private int totUniqPath(int row, int col,int[][] dp){
-        
-        if(row < 0 || col < 0) return 0;
-        if(row == 0  && col == 0)
-            return 1;
-        if(dp[row][col] != -1) return dp[row][col];
-        int count = 0;
-        
-        count += totUniqPath(row, col - 1, dp);//right
-
-        count += totUniqPath(row - 1, col, dp);//down
-
-        return dp[row][col] = count;
-    }	
 }

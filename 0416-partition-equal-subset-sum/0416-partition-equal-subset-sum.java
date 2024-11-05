d1c067ce158d61e1794773da6dead66970898897
Balasubramani E <138235393+DynamicDebugger@users.noreply.github.com>
2024-11-05 19:36:34 +0530

    Time: 45 ms (49.45%), Space: 51.3 MB (42.19%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -3,6 +3,28 @@ class Solution {
         int n = nums.length, target = 0;
         for(int i = 0 ; i < n ; i++) target += nums[i];
         if(target % 2 == 1) return false;
+        boolean[][] dp = new boolean[n][target + 1];
+        for(int i = 0 ; i < n ; i++) dp[i][0] = true;
+        dp[0][nums[0]] = true;
+        for(int ind = 1 ; ind  < n ; ind++){
+            for(int sum = 1; sum <= target/2 ; sum++){
+                
+                boolean pick = false; 
+                if(nums[ind] <= sum)pick = dp[ind - 1][sum - nums[ind]];
+                //not pick
+                boolean notPick = dp[ind - 1][sum];
+                
+                dp[ind][sum] = pick | notPick;                
+            }
+        }
+        return dp[n - 1][target/2];
+    }
+}
+/*
+    public boolean canPartition(int[] nums) {
+        int n = nums.length, target = 0;
+        for(int i = 0 ; i < n ; i++) target += nums[i];
+        if(target % 2 == 1) return false;
         Boolean[][] dp = new Boolean[n][target + 1];
         return subsetPartition(n - 1, target/2, nums, dp);
     }
@@ -19,5 +41,5 @@ class Solution {
         boolean notPick = subsetPartition(ind - 1,tar, arr, dp);
         
         return dp[ind][tar] = pick | notPick;
-    }    
-}
+    }   
+*/
